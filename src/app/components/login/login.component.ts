6db952d561c118e9f96d3788b68e62c2e082b3ad
Yordis <yordisponcealderete@gmail.com>
2023-04-17 23:53:44 -0400

    Added team work image

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,18 +11,26 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm: FormGroup
-  constructor(private authService: AuthService, private fb: FormBuilder) {
+  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
     })
   }
+  ngOnInit(): void {
+    if (this.authService.isLoggedIn()) {
+      this.redirectToHome();
+    }
+  }
 
   signIn() {
-    console.log(this.loginForm.value);
-
     this.authService.signIn();
+    this.redirectToHome();
+  }
+
+  redirectToHome() {
+    this.router.navigate(['/']);
   }
 }
